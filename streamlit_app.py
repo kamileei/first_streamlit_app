83b96575c00605b6d76ac60167eb9127b3e7e9f3
kamileei <102037474+kamileei@users.noreply.github.com>
2023-03-15 16:17:33 +0000

    Update streamlit_app.py
    import requests
    call fruityvice api

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import requests as r
 
 streamlit.title('My Parents New Healthy Diner')
 
@@ -20,3 +21,6 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
 
+#fruityvice API
+fruityvice_response = r.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
