2ccae36bf48db2771d8392fad320f6edc9a51417
kamileei <102037474+kamileei@users.noreply.github.com>
2023-03-15 16:23:57 +0000

    Update streamlit_app.py
    optimise fruit selection & api call

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,8 +23,10 @@ streamlit.dataframe(fruits_to_show)
 
 #fruityvice API
 streamlit.header('Fruityvice Fruit Advice!')
-fruityvice_response = r.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
 
+fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
+streamlit.write('The user entered', fruit_choice)
+
+fruityvice_response = r.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
