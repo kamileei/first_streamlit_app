0de412abe272a2896ccdaf1b5968e33e191ebab8
kamileei <102037474+kamileei@users.noreply.github.com>
2023-03-16 13:28:29 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,26 +53,22 @@ def get_fruit_load_list():
   with my_cnx.cursor() as my_cur:
     my_cur.execute("select * from fruit_load_list")
     return my_cur.fetchall()
+
   
-#add a button to load the fruit
-if streamlit.button('Get Fruit Load List'):
-  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-  my_data_rows = get_fruit_load_list()
-  streamlit.dataframe(my_data_rows)
-
-#don't run anything past here while I troubleshoot
-#streamlit.stop()
-
-new_fruit = streamlit.text_input('What fruit would you like to add?')
 
 def insert_row_snow(new_fruit):
   with my_cnx.cursor() as my_cur:
     my_cur.execute("insert into fruit_load_list values ('" + new_fruit +"')")
-    return "Thanks for adding" + new_fruit
-
+    return "Thanks for adding " + new_fruit
   
-if not new_fruit:
-  streamlit.error("Please select a fruit to add.")
-else:
-  streamlit.write(insert_row_snow(new_fruit))
+#add a button to load the fruit
+if streamlit.button('Get Fruit List'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_data_rows = get_fruit_load_list()
+  streamlit.dataframe(my_data_rows)
+  new_fruit = streamlit.text_input('What fruit would you like to add?')
+  if not new_fruit:
+    streamlit.error("Please select a fruit to add.")
+  else:
+    streamlit.write(insert_row_snow(new_fruit))
     
